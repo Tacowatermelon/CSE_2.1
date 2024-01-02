fd70d6c7c60f53780d2d3ff051957c4026d73cf6
L.Geisler <Lucas.geisler@vtsd.com>
2024-01-02 11:45:52 -0500

    Setup CSE 2.1 repo

--- 2.1.1/main.py
+++ 2.1.1/main.py
@@ -1,1 +1,14 @@
-print("test")
+count=1
+while count <=3:
+    print("Hello World")
+    count = count + 1
+
+def count_down():
+    global number
+    for number in range (11):
+        print (number)
+    return "Blast off!"
+print(count_down())
+
+name = input("What is your name?")
+print(name)
