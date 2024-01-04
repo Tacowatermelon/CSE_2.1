b865463d10034b93876979fa01d3223bb5823f5f
L.Geisler <Lucas.geisler@vtsd.com>
2024-01-04 13:10:38 -0500

    Setup CSE 2.1 repo

--- 2.1.1/main.py
+++ 2.1.1/main.py
@@ -15,12 +15,24 @@ print(name)
 
 import random
 
-number = random.randint(1, 10)
-guess = input("Guess an integer betweer 1 and 10 ")
+number = random.randint (1,10)
+guess = input("Guess an integer between 1 and 10 ")
 guess = int(guess)
 if number == guess:
     print("Correct!")
 else:
     print("Incorrect. " + "The number was " + str(number))
+
+
+import math
+
+radius = float(input("What is the radius of the tire?"))
+seconds = float(input("What is the time in seconds it traveled for?"))
+
+circumference = (2 * 3.14) * radius
+minutes = seconds / 60
+remainder = seconds % 60
+print("It traveled ", int(minutes), "minutes and ", remainder, " seconds.")
+print("The tire circumference is " + str(circumference))
 
 
